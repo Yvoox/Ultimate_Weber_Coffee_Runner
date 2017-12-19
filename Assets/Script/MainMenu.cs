5241ce4e30144a69fcdcd1228f8fb61e16fd7e52
Yvoox <alexandre-colicchio@hotmail.fr>
2017-12-19 09:44:01 +0100

    Started repo

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public string level;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void LaunchGame()
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
